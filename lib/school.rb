019fe1ed9f8c8f1c250c63de41c81ae9e7b2ca78
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-12-02 23:07:21 -0600

    commit tree fa3653c16737f75791bd95c6a82b66a28c777aed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+    def initialize(name)
+        @name = name
+        @roster= {}
+        @grade = 0
+    end
+
+    attr_accessor :name 
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+    
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        # sorted_hash = {}
+        @roster.map { |grade, name_array| [grade, name_array.sort] }.to_h
+        # sorted_hash
+    end
+
+    def roster
+        @roster
+    end
+
+end
